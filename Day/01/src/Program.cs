46b8a7fcdb80198a7380d94177cd045c64672b17
Christopher Kyle Horton <3268365+Xyaneon@users.noreply.github.com>
2024-12-08 16:20:58 -0500

    Day 1: Read puzzle input from a file path

--- Day/01/src/Program.cs
+++ Day/01/src/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Length != 1)
+{
+    Console.Error.WriteLine($"Incorrect number of arguments (expected 1, got {args.Length})");
+    Console.Error.WriteLine("Usage: dotnet Day1.exe input-file");
+    return 1;
+}
+
+string[] lines;
+
+try
+{
+    lines = File.ReadAllLines(args[0]);
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine($"Failed to read input file: {ex.Message}");
+    return 2;
+}
+
+
+return 0;
