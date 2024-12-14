9a786ea1ced53f62620a8109573e2d1ad65cd113
Christopher Kyle Horton <3268365+Xyaneon@users.noreply.github.com>
2024-12-13 20:58:41 -0500

    Day 5: Set up project

---
+++ Day/05/src/Program.cs
@@ -0,0 +1,20 @@
+﻿if (args.Length != 1)
+{
+    Console.Error.WriteLine($"Incorrect number of arguments (expected 1, got {args.Length})");
+    Console.Error.WriteLine("Usage: dotnet Day4.exe input-file");
+    return 1;
+}
+
+IReadOnlyList<string> lines;
+
+try
+{
+    lines = File.ReadAllLines(args[0]).AsReadOnly();
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine($"Failed to read input file: {ex.Message}");
+    return 2;
+}
+
+return 0;
